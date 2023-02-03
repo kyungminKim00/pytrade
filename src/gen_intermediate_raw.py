fa2aa5ddf1c479aaa3bb3fc58d7fe012dfdb158f
kmkim <kim.kyungmin00@gmail.com>
2023-02-03 09:18:03 +0000

    init

--- src/gen_intermediate_raw.py
+++ src/gen_intermediate_raw.py
@@ -35,122 +35,122 @@ sensingval_col = [
 #     return f"{col_name}_log", np.log(values)
 
 
-@ray.remote
-def mp_moving_averages(values: np, w_size: int = None, candle_size: int = None) -> List:
-    return f"candle{str(candle_size)}_ma{str(w_size)}", bn.move_mean(
-        values, window=w_size * candle_size
-    )
+# @ray.remote
+# def mp_moving_averages(values: np, w_size: int = None, candle_size: int = None) -> List:
+#     return f"candle{str(candle_size)}_ma{str(w_size)}", bn.move_mean(
+#         values, window=w_size * candle_size
+#     )
 
 
-@ray.remote
-def mp_gen_candles2(
-    partial_pd: pd.DataFrame,
-    min_range: List,
-) -> List:
-    candle_time_interval = None
-    first_cursor = None
-    last_cursor = None
+# @ray.remote
+# def mp_gen_candles2(
+#     partial_pd: pd.DataFrame,
+#     min_range: List,
+# ) -> List:
+#     candle_time_interval = None
+#     first_cursor = None
+#     last_cursor = None
 
-    # declare last_cursor
-    partial_pd_idxs = partial_pd.index
-    # partial_pd_idxs = sorted(list(partial_pd.keys()))
-    last_cursor = partial_pd_idxs[-1]
+#     # declare last_cursor
+#     partial_pd_idxs = partial_pd.index
+#     # partial_pd_idxs = sorted(list(partial_pd.keys()))
+#     last_cursor = partial_pd_idxs[-1]
 
-    # # find candle_time_interval
-    x_mins = int(partial_pd.loc[last_cursor]["mins"])
-    # x_mins = int(partial_pd[last_cursor]["mins"])
+#     # # find candle_time_interval
+#     x_mins = int(partial_pd.loc[last_cursor]["mins"])
+#     # x_mins = int(partial_pd[last_cursor]["mins"])
 
-    for v in min_range:
-        if v[0] <= x_mins < v[1]:
-            candle_time_interval = v
-    assert candle_time_interval is not None, "Data Errors!!!"
+#     for v in min_range:
+#         if v[0] <= x_mins < v[1]:
+#             candle_time_interval = v
+#     assert candle_time_interval is not None, "Data Errors!!!"
 
-    # find first_cursor
-    for t_idx in partial_pd_idxs:
-        x_mins = int(partial_pd.loc[t_idx]["mins"])
-        # x_mins = int(partial_pd[last_cursor]["mins"])
-        if candle_time_interval[0] <= x_mins < candle_time_interval[1]:
-            first_cursor = t_idx
-            break
-    assert first_cursor is not None, "Data Errors!!!"
+#     # find first_cursor
+#     for t_idx in partial_pd_idxs:
+#         x_mins = int(partial_pd.loc[t_idx]["mins"])
+#         # x_mins = int(partial_pd[last_cursor]["mins"])
+#         if candle_time_interval[0] <= x_mins < candle_time_interval[1]:
+#             first_cursor = t_idx
+#             break
+#     assert first_cursor is not None, "Data Errors!!!"
+# 
+    # return ohlc_record(partial_pd, first_cursor, last_cursor)
 
-    return ohlc_record(partial_pd, first_cursor, last_cursor)
 
-
-def ohlc_record(partial_pd, first_cursor, last_cursor):
-    candle_open = partial_pd.loc[first_cursor]["open"]
-    candle_high = partial_pd.loc[first_cursor:]["high"].max()
-    candle_low = partial_pd.loc[first_cursor:]["low"].min()
-    candle_close = partial_pd.loc[last_cursor]["close"]
+# def ohlc_record(partial_pd, first_cursor, last_cursor):
+#     candle_open = partial_pd.loc[first_cursor]["open"]
+#     candle_high = partial_pd.loc[first_cursor:]["high"].max()
+#     candle_low = partial_pd.loc[first_cursor:]["low"].min()
+#     candle_close = partial_pd.loc[last_cursor]["close"]
 
     # candle_open = partial_pd[first_cursor]["open"]
     # candle_high = partial_pd[first_cursor:]["high"].max()
     # candle_low = partial_pd[first_cursor:]["low"].min()
     # candle_close = partial_pd[last_cursor]["close"]
 
-    data = [last_cursor, candle_open, candle_high, candle_low, candle_close]
-    return data
+    # data = [last_cursor, candle_open, candle_high, candle_low, candle_close]
+    # return data
 
 
-@ray.remote
-def mp_gen_candles(values: np, max_time: int, sub_window_size: int) -> np:
-    stride_size = sub_window_size
-    sub_windows = (
-        np.expand_dims(np.arange(sub_window_size), 0)
-        + np.expand_dims(np.arange(max_time + 1), 0).T
-    )
-    sub_windows = sub_windows[::stride_size]
+# @ray.remote
+# def mp_gen_candles(values: np, max_time: int, sub_window_size: int) -> np:
+#     stride_size = sub_window_size
+#     sub_windows = (
+#         np.expand_dims(np.arange(sub_window_size), 0)
+#         + np.expand_dims(np.arange(max_time + 1), 0).T
+#     )
+#     sub_windows = sub_windows[::stride_size]
 
-    add_dummy = None
-    eov = sub_windows[-1][-1]  # the end of value
-    if (max_time - 1) < eov:
-        add_dummy = True
-        eor = sub_windows[-1]  # the end of rows
-        sub_windows = sub_windows[:-1]
-    else:
-        add_dummy = False
+#     add_dummy = None
+#     eov = sub_windows[-1][-1]  # the end of value
+#     if (max_time - 1) < eov:
+#         add_dummy = True
+#         eor = sub_windows[-1]  # the end of rows
+#         sub_windows = sub_windows[:-1]
+#     else:
+#         add_dummy = False
 
-    # open_val = np.array(
-    #     [
-    #         it
-    #         for it in list(
-    #             values[sub_windows][:, 0, 0] for roof in range(sub_window_size)
-    #         )
-    #     ]
-    # ).T.flatten()
-    open_val = np.array(
-        list(list((values[sub_windows][:, 0, 0] for roof in range(sub_window_size))))
-    ).T.flatten()
+#     # open_val = np.array(
+#     #     [
+#     #         it
+#     #         for it in list(
+#     #             values[sub_windows][:, 0, 0] for roof in range(sub_window_size)
+#     #         )
+#     #     ]
+#     # ).T.flatten()
+#     open_val = np.array(
+#         list(list((values[sub_windows][:, 0, 0] for roof in range(sub_window_size))))
+#     ).T.flatten()
 
-    max_val = np.array(
-        [
-            it
-            for it in list(map(np.max, values[sub_windows]))
-            for roof in range(sub_window_size)
-        ]
-    ).T.flatten()
-    min_val = np.array(
-        [
-            it
-            for it in list(map(np.min, values[sub_windows]))
-            for roof in range(sub_window_size)
-        ]
-    ).T.flatten()
-    close_val = np.array(
-        [
-            it
-            for it in list(values[sub_windows][:, -1, -1])
-            for roof in range(sub_window_size)
-        ]
-    ).T.flatten()
-    # data = np.array([it for it in [open_val, max_val, min_val, close_val]]).T
-    data = np.array([open_val, max_val, min_val, close_val]).T
+#     max_val = np.array(
+#         [
+#             it
+#             for it in list(map(np.max, values[sub_windows]))
+#             for roof in range(sub_window_size)
+#         ]
+#     ).T.flatten()
+#     min_val = np.array(
+#         [
+#             it
+#             for it in list(map(np.min, values[sub_windows]))
+#             for roof in range(sub_window_size)
+#         ]
+#     ).T.flatten()
+#     close_val = np.array(
+#         [
+#             it
+#             for it in list(values[sub_windows][:, -1, -1])
+#             for roof in range(sub_window_size)
+#         ]
+#     ).T.flatten()
+#     # data = np.array([it for it in [open_val, max_val, min_val, close_val]]).T
+#     data = np.array([open_val, max_val, min_val, close_val]).T
 
-    if add_dummy:
-        dummy = values[eor[eor < max_time]]
-        data = np.append(data, dummy, axis=0)
+#     if add_dummy:
+#         dummy = values[eor[eor < max_time]]
+#         data = np.append(data, dummy, axis=0)
 
-    return f"{sub_window_size}mins", data
+    # return f"{sub_window_size}mins", data
 
 
 class RawDataReader:
@@ -190,95 +190,95 @@ class RawDataReader:
         # 추후 삭제 - 코드개발시 데이터 사이즈 줄이기 위해 존재하는 코드
         r_pd = r_pd[-2000:]
         print_c("Reduce the size of raw data - Remove this section")
-
+        
         # generate datetime
         r_pd["datetime"] = pd.to_datetime(
             r_pd["date"] + " " + r_pd["hours"] + ":" + r_pd["mins"]
         )
 
-        # 리파인 raw 데이터 이평 추가
-        r_pd = self._moving_averages(r_pd)
+        # 리파인 raw 1분갱신 캔들가격과 캔들이평 추가
+        r_pd = self._generate_candle_price(r_pd)
 
         # 실제 쓰이는 분봉 데이터 생성을 위해 00분에 맞추어서 데이터 재조정
         r_pd = self._calibrate_min(r_pd)
 
-        # 분봉 데이터 생성
-        r_pd = self._gen_candles2(r_pd)
+        # # 분봉 데이터 생성
+        # r_pd = self._gen_candles2(r_pd)
 
         # # 로그 변환
         # r_pd = self._log_transformation(r_pd)
 
         return r_pd
 
-    def _gen_candles(self, r_pd: pd.DataFrame) -> pd.DataFrame:
-        values = r_pd[["open", "high", "low", "close"]].values
+    # def _gen_candles(self, r_pd: pd.DataFrame) -> pd.DataFrame:
+    #     values = r_pd[["open", "high", "low", "close"]].values
 
-        max_time = values.shape[0]
-        res = ray.get(
-            [
-                mp_gen_candles.remote(values, max_time, sub_window_size)
-                for sub_window_size in self.candle_size[1:]
-            ]
-        )
-        for k, v in res:
-            r_pd = pd.concat(
-                [
-                    r_pd,
-                    pd.DataFrame(
-                        data=v,
-                        columns=[f"{k}_open", f"{k}_high", f"{k}_low", f"{k}_close"],
-                        index=r_pd.index,
-                    ),
-                ],
-                axis=1,
-                join="inner",
-            )
-        print("Gen Candles: Done")
-        return r_pd
+    #     max_time = values.shape[0]
+    #     res = ray.get(
+    #         [
+    #             mp_gen_candles.remote(values, max_time, sub_window_size)
+    #             for sub_window_size in self.candle_size[1:]
+    #         ]
+    #     )
+    #     for k, v in res:
+    #         r_pd = pd.concat(
+    #             [
+    #                 r_pd,
+    #                 pd.DataFrame(
+    #                     data=v,
+    #                     columns=[f"{k}_open", f"{k}_high", f"{k}_low", f"{k}_close"],
+    #                     index=r_pd.index,
+    #                 ),
+    #             ],
+    #             axis=1,
+    #             join="inner",
+    #         )
+    #     print("Gen Candles: Done")
+    #     return r_pd
 
-    def _gen_candles2(self, r_pd: pd.DataFrame) -> pd.DataFrame:
-        r_pd.to_csv("./src/local_data/raw/gen_candles_debug_source.csv")
-        for candle_size in self.candle_size[1:]:
-            min_range = list(get_min_range(candle_size).values())
+    # def _gen_candles2(self, r_pd: pd.DataFrame) -> pd.DataFrame:
+    #     r_pd.to_csv("./src/local_data/raw/gen_candles_debug_source.csv")
+    #     for candle_size in self.candle_size[1:]:
+    #         min_range = list(get_min_range(candle_size).values())
 
-            # 3/5/15 분봉의 시고저종 산출
-            res = ray.get(
-                [
-                    mp_gen_candles2.remote(
-                        r_pd.loc[cursor - candle_size : cursor],
-                        min_range,
-                    )
-                    for cursor in list(
-                        r_pd.index[candle_size:]
-                    )  # cursor 포함되는 데이터여야 함 (인덱스 이므로)
-                ]
-            )
+    #         # 3/5/15 분봉의 시고저종 산출
+    #         res = ray.get(
+    #             [
+    #                 mp_gen_candles2.remote(
+    #                     r_pd.loc[cursor - candle_size : cursor],
+    #                     min_range,
+    #                 )
+    #                 for cursor in list(
+    #                     r_pd.index[candle_size:]
+    #                 )  # cursor 포함되는 데이터여야 함 (인덱스 이므로)
+    #             ]
+    #         )
 
-            # 계산된 3/5/15 분봉의 시고저종을 원본데이터 프레임에 추가
-            identifier = f"{candle_size}mins"
-            res_dict = {
-                idx: {
-                    f"{identifier}_open": o,
-                    f"{identifier}_high": h,
-                    f"{identifier}_low": l,
-                    f"{identifier}_close": c,
-                }
-                for idx, o, h, l, c in res
-            }
-            res_pd = pd.DataFrame.from_dict(res_dict, orient="index")
-            r_pd = pd.concat(
-                [
-                    r_pd,
-                    res_pd,
-                ],
-                axis=1,
-                join="inner",
-            )
-            print(f"{identifier} 캔들이 생성 되었습니다")
+    #         # 계산된 3/5/15 분봉의 시고저종을 원본데이터 프레임에 추가
+    #         identifier = f"{candle_size}mins"
+    #         res_dict = {
+    #             idx: {
+    #                 f"{identifier}_open": o,
+    #                 f"{identifier}_high": h,
+    #                 f"{identifier}_low": l,
+    #                 f"{identifier}_close": c,
+    #             }
+    #             for idx, o, h, l, c in res
+    #         }
+    #         res_pd = pd.DataFrame.from_dict(res_dict, orient="index")
+    #         r_pd = pd.concat(
+    #             [
+    #                 r_pd,
+    #                 res_pd,
+    #             ],
+    #             axis=1,
+    #             join="inner",
+    #         )
+    #         print(f"{identifier} 캔들이 생성 되었습니다")
 
-        r_pd.to_csv("./src/local_data/raw/gen_candles_debug_target.csv")
+    #     r_pd.to_csv("./src/local_data/raw/gen_candles_debug_target.csv")
 
-        return r_pd
+    #     return r_pd
 
     def _calibrate_min(self, r_pd: pd.DataFrame) -> pd.DataFrame:
         first_index = r_pd.index[0]
@@ -286,7 +286,7 @@ class RawDataReader:
         print("Calibrate Min: Done")
         return r_pd.drop(labels=range(first_index, first_00min_index), axis=0)
 
-    def _moving_averages(self, r_pd: pd.DataFrame) -> pd.DataFrame:
+    def _generate_candle_price(self, r_pd: pd.DataFrame) -> pd.DataFrame:
         # # group by code
         # https://teddylee777.github.io/pandas/pandas-groupby/
 
@@ -331,7 +331,7 @@ class RawDataReader:
         #     r_pd[k] = v
 
         r_pd = r_pd.dropna(axis=0)
-        print("Gen MA: Done")
+        print("Gen Candle prices: Done")
         return r_pd
 
     def pd_formatting(self, data):
